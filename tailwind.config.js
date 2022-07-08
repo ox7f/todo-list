c6e2bb28017c91434d233bcb88fd61721117787b
ox7f <ox7f@protonmail.com>
2022-07-08 18:45:02 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    safelist: [
+        {
+            pattern: /./
+        },
+    ],
+    theme: {
+            extend: {},
+    },
+    plugins: [
+            require('daisyui'),
+    ],
+}
