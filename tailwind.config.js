1abb5e21fcee7ab4c97a3d0a7ff5a7855a51714c
ox7f <ox7f@protonmail.com>
2022-07-09 15:49:26 +0200

    config stuff and formatting

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+    content: ["node_modules/daisyui/dist/**/*.js"],
+    plugins: [require("daisyui")],
+    safelist: [
+        {
+            pattern: /./
+        },
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+}
